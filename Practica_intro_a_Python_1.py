147228506a69f82a4c7589eacfdf2300faa6a8d0
sol <sol.bertinat12@gmail.com>
2022-04-13 14:29:59 -0300

    terminado

--- Practica_intro_a_Python_1.py
+++ Practica_intro_a_Python_1.py
@@ -1,35 +1,35 @@
 #1 - Escribir un programa que imprima la longitud de un string el cual se lee por teclado.
-    #inp = input("ingrese una frase: ")
-    #print(len(inp))
+inp = input("ingrese una frase: ")
+print(len(inp))
 
 #2 - Realizar un programa donde se imprima la 5ta y 6ta letra de un string pasado por teclado 
 # en mayúscula (Asegurarse de que el string tenga la cantidad de caracteres suficientes).
-#inp = input("ingrese una frase ")
-#if (len(inp) >= 6):
-#    print(str.upper(inp[4:6]))
-#else:
-#    print("su frase no tiene suficiente caracteres, por favor intente nuevamente")
+inp = input("ingrese una frase ")
+if (len(inp) >= 6):
+    print(str.upper(inp[4:6]))
+else:
+    print("su frase no tiene suficiente caracteres, por favor intente nuevamente")
 
 #3 - Escribir un programa que pregunte el nombre y apellido al usuario y lo salude.
-    #inp = input('ingrese su nombre y apellido: ')
-    #print('hola, ', inp)
+inp = input('ingrese su nombre y apellido: ')
+print('hola, ', inp)
 
 #4 - Pedir el nombre y los dos apellidos de una persona y mostrar las iniciales en mayúsculas
-    #nombre = input("Ingrese su nombre ")
-    #apellido1 = input("Ingrese su primer apellido ")
-    #apellido2 = input("Ingrese su segundo apellido ")
-    #print(str.upper(nombre[0]),str.upper(apellido1[0]),str.upper(apellido2[0]))
+nombre = input("Ingrese su nombre ")
+apellido1 = input("Ingrese su primer apellido ")
+apellido2 = input("Ingrese su segundo apellido ")
+print(str.upper(nombre[0]),str.upper(apellido1[0]),str.upper(apellido2[0]))
 
 #5 - Realizar un programa que lea tres números por teclado y calcule el promedio de ellos.
-# numeros = input("Ingrese tres números (separados por un espacio) ")
-# lista = numeros.split(" ")
-# promedio = (int(lista[0]) + int(lista[1]) + int(lista[2]))/len(lista)
-# print(promedio)
+numeros = input("Ingrese tres números (separados por un espacio) ")
+lista = numeros.split(" ")
+promedio = (int(lista[0]) + int(lista[1]) + int(lista[2]))/len(lista)
+print(promedio)
 
 #6 - Dada una cierta cantidad de minutos (ingresada por teclado) hacer un 
 # programa que muestre cuántas horas y minutos son. Por ejemplo 150 minutos son 2 horas y 30 minutos.
-#minutos = int(input("Ingrese una cantidad de minutos "))
-#print(minutos//60, " horas y ", (minutos%60), " minutos")
+minutos = int(input("Ingrese una cantidad de minutos "))
+print(minutos//60, " horas y ", (minutos%60), " minutos")
 
 #7 - Un comerciante, el cual tiene un sueldo base, recibe un 10% extra por comisión por cada 
 # venta que realiza. Realizar un programa que devuelva el dinero que recibirá por comisión luego 
@@ -43,15 +43,15 @@ def dinero_fin_mes(sueldo_base, n):
         contador += 1
     print("total dinero por comisiones: $" + str(dinero_ventas))
     print("total dinero ganado en el mes: $" + str((dinero_ventas + sueldo_base)))
-    
+
 dinero_fin_mes(100, 4)
 
 #8 - Escribir un programa para calcular la nota final de un estudiante, teniendo en cuenta que por cada 
 # respuesta correcta el estudiante suma 4 puntos, por cada incorrecta se le resta 1 punto y si la respuesta 
 # está en blanco no se le suma ni se le resta.
-#cant_preguntas = int(input("cantidad de preguntas: "))
-#rtas_correctas = int(input("respuestas correctas: "))
-#rtas_incorrectas = int(input("respuestas incorrectas: "))
-#nota = rtas_correctas*4 - rtas_incorrectas
-#print("la nota final del alumno es ", nota, "/", cant_preguntas*4)
+cant_preguntas = int(input("cantidad de preguntas: "))
+rtas_correctas = int(input("respuestas correctas: "))
+rtas_incorrectas = int(input("respuestas incorrectas: "))
+nota = rtas_correctas*4 - rtas_incorrectas
+print("la nota final del alumno es ", nota, "/", cant_preguntas*4)
 
