f62ffb890617a5c94ef1bb7b39be2a7c725f65c1
sol <sol.bertinat12@gmail.com>
2022-04-24 17:23:37 -0300

    actualizacion 24/4 17: 23

--- POO/Practica/guia_POO_parte1.py
+++ POO/Practica/guia_POO_parte1.py
@@ -95,3 +95,89 @@
 
 #     def ultimoComando (self):
 #         print(self.comando)
+
+#6 - Implementá una clase que represente una calculadora sencilla, que permita sumar, restar y multiplicar. 
+# Este objeto debe entender los siguientes mensajes: cargar(numero), sumar(numero), restar(numero), 
+# multiplicar(numero) y valorActual()
+# class Calculadora:
+#     def __init__(self):
+#         self.numero = 0
+
+#     def cargar(self, carga):
+#         self.numero = carga
+
+#     def sumar(self, suma):
+#         self.numero += suma
+
+#     def restar(self, resta):
+#         self.numero -= resta
+
+#     def multiplicar(self, multiplicacion):
+#         self.numero *= multiplicacion
+    
+#     def valorActual(self):
+#         print(self.numero)
+
+# calculadora = Calculadora()
+# calculadora.cargar(0)
+# calculadora.sumar(4)
+# calculadora.multiplicar(5)
+# calculadora.restar(8)
+# calculadora.multiplicar(2)
+# calculadora.valorActual() #24
+
+#7 - Definí una clase de gorriones, de los cuales nos interesa conocer dos medidas conocidas como 
+# CSS (coeficiente de serenidad silenciosa), CSSP y CSSV (como el CSS pero “pico” y “veces”). El 
+# CSS resulta de dividir la cantidad total de kilómetros que vuela desde que se lo comienza a estudiar, 
+# por la cantidad total de gramos de comida que ingiere. El CSSP es la misma división pero considerando 
+# solamente lo que voló la vez que más voló y lo que comió la vez que más comió. El CSSV es otra vez la 
+# misma idea, respecto de la cantidad de veces que voló y comió. Si un gorrión nunca comió, los 
+# coeficientes deben ser None. Un gorrión se considera en equilibrio si su CSS está entre 0.5 y 2.
+# class Gorriones:
+#     def __init__ (self):
+#         self.gramos = 0
+#         self.kms = 0
+#         self.vuelos = []
+#         self.comidas = []
+
+#     def volar(self, kms):
+#         self.kms += kms
+#         self.vuelos.append(kms)
+
+#     def comer(self, gramos):
+#         self.gramos += gramos
+#         self.comidas.append(gramos)
+
+#     def CSS(self):
+#         if self.gramos <= 0:
+#             return None
+#         else:
+#             return self.kms/self.gramos
+
+#     def CSSP(self):
+#         if self.gramos <= 0:
+#             return None
+#         else:
+#             return int(max(self.vuelos))/int(max(self.comidas))
+
+#     def CSSV(self):
+#         if self.gramos <= 0:
+#             return None
+#         else:
+#             return len(self.vuelos)/len(self.comidas)
+    
+#     def enEquilibrio(self):
+#         if(self.CSS() > 0,5 and self.CSS() < 2):
+#             print("esta en equilibrio")
+#         else:
+#             print("no esta en equilibrio")
+        
+# lila = Gorriones()
+# lila.comer(35)
+# lila.volar(80)
+# lila.comer(70)
+# lila.volar(120)
+# print(lila.CSS())
+# print(lila.CSSP())
+# print(lila.CSSV())
+# print(lila.enEquilibrio())
